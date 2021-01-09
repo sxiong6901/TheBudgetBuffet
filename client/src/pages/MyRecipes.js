9f1a129e8d8320236b2c92e0066ed853c46cc228
Shawanda Ford <shawanda.ford@gmail.com>
2021-01-09 13:38:25 -0500

    adding the functionality to searchbox on /viewall route

--- client/src/pages/MyRecipes.js
+++ client/src/pages/MyRecipes.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+// import { myRecipes, myFavorites } from '../../../controllers'
 import RecipeCard from '../components/RecipeCard'
 import API from '../utils/API'
 
@@ -6,7 +7,7 @@ import API from '../utils/API'
 const MyRecipes = props => {
 	const [recipes, setRecipes] = useState([])
 	const [recipe, setNewRecipe] = useState([])
-	const [favoriteRecipes, setFavoriteRecipes] = useState([])
+	// const [favoriteRecipes, setFavoriteRecipes] = useState([])
 	// useEffect(()=> {
 	// 	API.myFavorites()
 	// 	.then(results=> {
@@ -45,32 +46,33 @@ const MyRecipes = props => {
 		console.log(newRecipeList)
 
 
-	}
+	// }
 
-	const save = (id) => {
-		console.log(id)
-
-		var favoritesList = []
-		recipes.forEach(element => {
-			if (element._id === id) {
-				favoritesList.push(element)
-				// setFavoriteRecipes(favoritesList)
-				// console.log(favoritesList)
-			}
-
-		})
+	
+	// const myFavorites = (id) => {
+	// 	console.log(id)
+	// 	API.myRecipe(id)
+	// 		.then(res => loadRecipe(res))
+	// 		.catch(err => console.log(err));
+	// 		console.log(recipe)
+		// var myfavorites = recipes.push(recipe => recipe._id !== id)
+		// setRecipes(myfavorites)
+		// console.log(myfavorites)
 
 
 	}
+
+
+	
 
 
 	return (
 		<>
 			<h1>My Recipes</h1>
 
-			{recipes && recipes.map(recipe => <RecipeCard key={recipe._id} recipe={recipe} removeRecipe={updateRecipe} saveRecipe={save}></RecipeCard>)}
+			{recipes && recipes.map(recipe => <RecipeCard key={recipe._id} recipe={recipe} removeRecipe={updateRecipe}></RecipeCard>)}
 		</>
 	)
-}
 
+	}
 export default MyRecipes
